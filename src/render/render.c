b0886c63889e201120c73ca83e99e0e2c8a5b6f8
Thibaud Maitre <thmaitre@z2r5p5.42lyon.fr>
2025-09-29 22:29:53 +0200

    first branch push

--- src/render/render.c
+++ src/render/render.c
@@ -6,7 +6,7 @@
 /*   By: thmaitre <thmaitre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:49:13 by thmaitre          #+#    #+#             */
-/*   Updated: 2025/09/29 20:14:05 by thmaitre         ###   ########.fr       */
+/*   Updated: 2025/09/29 22:27:37 by thmaitre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,9 @@ int	**create_map(void)
 		{ 1 ,0, 0, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1 ,0, 0, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1 ,0, 0, 0, 0, 0, 0, 0, 0, 1 },
-		{ 1 ,0, 0, 0, 0, 0, 0, 0, 0, 1 },
-		{ 1 ,0, 0, 0, 0, 0, 0, 0, 0, 1 },
-		{ 1 ,0, 0, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1 ,0, 0, 0, 1, 0, 0, 0, 0, 1 },
+		{ 1 ,0, 0, 0, 1, 0, 0, 0, 0, 1 },
+		{ 1 ,0, 0, 0, 1, 0, 0, 0, 0, 1 },
 		{ 1 ,0, 0, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1 ,0, 0, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1 },
@@ -65,22 +65,81 @@ int	**create_map(void)
 	return (map);
 }
 
-int	render(t_mlx_data *mlx_data)
+int	render_box(t_mlx_data *mlx_data, int mapX, int mapY, int color)
 {
+	int	box_w;
+	int	box_h;
 	int	x;
 	int	y;
-	int	**map;
-	int	color;
+
+	box_w = mlx_data->mlx_img->width / 10;
+	box_h = mlx_data->mlx_img->height / 10;
+	x = 0;
+	y = 0;
+	while (y < mlx_data->mlx_img->height)
+	{
+		x = 0;
+		while (x < mlx_data->mlx_img->width)
+		{
+			if (x >= mapX * box_w && x <= mapX * box_w + box_w)
+				if (y >= mapY * box_h && y <= mapY * box_h + box_h)
+					put_one_pixel(mlx_data->mlx_img, x, y, color);
+			if (x == mapX * box_w)
+				put_one_pixel(mlx_data->mlx_img, x, y, 0xFFFFFF);
+			if (y == mapY * box_h)
+				put_one_pixel(mlx_data->mlx_img, x, y, 0xFFFFFF);
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
+int	player(t_mlx_data *mlx_data)
+{
+	double	p_pos_x;
+	double	p_pos_y;
+
+	p_pos_x = 250;
+	p_pos_y = 750;
+	put_one_pixel(mlx_data->mlx_img, p_pos_x, p_pos_y, 0xFF0000);
+	put_one_pixel(mlx_data->mlx_img, p_pos_x + 1, p_pos_y + 1, 0xFF0000);
+	put_one_pixel(mlx_data->mlx_img, p_pos_x + 1, p_pos_y, 0xFF0000);
+	put_one_pixel(mlx_data->mlx_img, p_pos_x, p_pos_y + 1, 0xFF0000);
+	return (0);
+}
+
+int	render(t_mlx_data *mlx_data)
+{
+	int		x;
+	int		y;
+	int		**map;
+	int		wall_color;
+	int		floor_color;
 
 	x = 0;
 	y = 0;
 	map = create_map();
-	color = mix_color(150, 120, 45);
-	while (x < 1000)
+	wall_color = mix_color(42, 44, 101);
+	floor_color = mix_color(5, 3, 2);
+
+	while (y < 10)
 	{
-		put_one_pixel(mlx_data->mlx_img, x, 50, color);
-		x++;
+		x = 0;
+		while (x < 10)
+		{
+			if (map[y][x] == 1)
+				render_box(mlx_data, x, y, wall_color);
+			if (map[y][x] == 0)
+				render_box(mlx_data, x, y, floor_color);
+			x++;
+		}
+		y++;
 	}
-	mlx_put_image_to_window(mlx_data->mlx_ptr, mlx_data->win_ptr, mlx_data->mlx_img->img_ptr, 0, 0);
+
+	player(mlx_data);
+
+	mlx_put_image_to_window(mlx_data->mlx_ptr,
+		mlx_data->win_ptr, mlx_data->mlx_img->img_ptr, 0, 0);
 	return (0);
 }
