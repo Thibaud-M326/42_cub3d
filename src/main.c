292fefe08ee22c10f527cd1be1e6f02ce9f42224
6-6-Syj <magand.jessy@outlook.com>
2025-10-01 19:27:37 +0200

    FIX: 2 main

--- src/main.c
+++ src/main.c
@@ -3,27 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thmaitre <thmaitre@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmagand <jmagand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 18:03:32 by jmagand           #+#    #+#             */
-/*   Updated: 2025/10/01 19:15:07 by thmaitre         ###   ########.fr       */
+/*   Updated: 2025/10/01 19:26:51 by jmagand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
 #include "libft.h"
 #include "mlx.h"
-#include <stdio.h>
 
-int	main(void)
+// int	main(void)
+// {
+// 	t_data	*data;
+
+// 	data = init_data_struct();
+// 	// parse_input(ac, av, data);
+// 	if (!init_mlx(data))
+// 		free_and_exit(data, msg_custom("Error:\ninit_mlx failed"), 1);
+// 	render(data);
+// 	deploy_mlx_hook(data);
+// 	return (0);
+// }
+
+int	main(int ac, char **av)
 {
 	t_data	*data;
 
 	data = init_data_struct();
-	// parse_input(ac, av, data);
-	if (!init_mlx(data))
-		free_and_exit(data, msg_custom("Error:\ninit_mlx failed"), 1);
-	render(data);
-	deploy_mlx_hook(data);
+	parse_input(ac, av, data);
+	// if (!init_mlx(data))
+	// 	free_and_exit(data, msg_custom("Error:\ninit_mlx failed"), 1);
+	// render(data);
+	// deploy_mlx_hook(data);
 	return (0);
 }
