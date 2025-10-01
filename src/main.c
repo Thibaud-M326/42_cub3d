da3ef75fb09d8383a5a254ded65e526d3392c140
Thibaud Maitre <thmaitre@z2r5p5.42lyon.fr>
2025-10-01 19:29:32 +0200

    CHANGED: main

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmagand <jmagand@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thmaitre <thmaitre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 18:03:32 by jmagand           #+#    #+#             */
-/*   Updated: 2025/10/01 19:26:51 by jmagand          ###   ########.fr       */
+/*   Updated: 2025/10/01 19:28:48 by thmaitre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 #include "libft.h"
 #include "mlx.h"
 
-// int	main(void)
-// {
-// 	t_data	*data;
-
-// 	data = init_data_struct();
-// 	// parse_input(ac, av, data);
-// 	if (!init_mlx(data))
-// 		free_and_exit(data, msg_custom("Error:\ninit_mlx failed"), 1);
-// 	render(data);
-// 	deploy_mlx_hook(data);
-// 	return (0);
-// }
-
-int	main(int ac, char **av)
+int	main(void)
 {
 	t_data	*data;
 
 	data = init_data_struct();
-	parse_input(ac, av, data);
-	// if (!init_mlx(data))
-	// 	free_and_exit(data, msg_custom("Error:\ninit_mlx failed"), 1);
-	// render(data);
-	// deploy_mlx_hook(data);
+	// parse_input(ac, av, data);
+	if (!init_mlx(data))
+		free_and_exit(data, msg_custom("Error:\ninit_mlx failed"), 1);
+	render(data);
+	deploy_mlx_hook(data);
 	return (0);
 }
+
+// int	main(int ac, char **av)
+// {
+// 	t_data	*data;
+
+// 	data = init_data_struct();
+// 	parse_input(ac, av, data);
+// 	// if (!init_mlx(data))
+// 	// 	free_and_exit(data, msg_custom("Error:\ninit_mlx failed"), 1);
+// 	// render(data);
+// 	// deploy_mlx_hook(data);
+// 	return (0);
+// }
