cc276c6a192a50a861f91877580256e11bbc74e7
Thibaud Maitre <thmaitre@z2r5p5.42lyon.fr>
2025-10-01 18:52:42 +0200

    WIP: hook2

--- src/hook/hook.c
+++ src/hook/hook.c
@@ -6,7 +6,7 @@
 /*   By: thmaitre <thmaitre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 18:31:37 by thmaitre          #+#    #+#             */
-/*   Updated: 2025/10/01 18:45:07 by thmaitre         ###   ########.fr       */
+/*   Updated: 2025/10/01 18:48:50 by thmaitre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	handle_cross(t_data *data)
 {
-	free_and_exit(data, (t_msg)"", 0);
+	free_and_exit(data, "", 0);
 	return (0);
 }
 
