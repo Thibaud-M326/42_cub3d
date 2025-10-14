47cefdd069362800bcea4531e3308891dde0173c
Thibaud Maitre <thmaitre@z2r3p3.42lyon.fr>
2025-10-14 14:52:36 +0200

    WIP: load_textures

--- src/render/raycasting.c
+++ src/render/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: thmaitre <thmaitre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 14:15:01 by thmaitre          #+#    #+#             */
-/*   Updated: 2025/10/13 14:27:22 by thmaitre         ###   ########.fr       */
+/*   Updated: 2025/10/14 14:29:03 by thmaitre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int	raycasting(t_data *data)
 		camera_x = 2 * x / (double)1000 - 1;
 		data->player->ray.dir_x = data->player->dir_x + plane_x * camera_x;
 		data->player->ray.dir_y = data->player->dir_y + plane_y * camera_x;
-        raycasting_init(data);
-        raycasting_dda(data);
+		raycasting_init(data);
+		raycasting_dda(data);
 		draw_vertical_line(data, x);
 		x++;
 	}
--- src/texture/load_textures.c
+++ src/texture/load_textures.c
@@ -6,11 +6,14 @@
 /*   By: thmaitre <thmaitre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 18:53:03 by thmaitre          #+#    #+#             */
-/*   Updated: 2025/10/13 19:43:20 by thmaitre         ###   ########.fr       */
+/*   Updated: 2025/10/14 14:48:18 by thmaitre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
+#include "mlx.h"
+
+	#include <stdio.h>
 
 int	load_texture_img(t_data *data, t_mlx_img *tex, char card)
 {
@@ -24,24 +27,25 @@ int	load_texture_img(t_data *data, t_mlx_img *tex, char card)
 		tex_path = data->textures->path_w;
 	if (card == 't')
 		tex_path = data->textures->path_s;
+	printf("tex_path : %s\n", tex_path);
 	tex->img_ptr = mlx_xpm_file_to_image(
 			data->mlx_data->mlx_ptr,
 			tex_path,
-			tex->width,
-			tex->height);
+			&tex->width,
+			&tex->height);
 	tex->img_data = mlx_get_data_addr(
 			tex->img_ptr,
-			tex->bits_per_pixel,
-			tex->size_line,
-			tex->endian);
+			&tex->bits_per_pixel,
+			&tex->size_line,
+			&tex->endian);
 	return (1);
 }
 
 int	load_textures(t_data *data)
 {
-	load_texture(data, data->textures->tex_n, 'n');
-	load_texture(data, data->textures->tex_e, 'e');
-	load_texture(data, data->textures->tex_w, 'w');
-	load_texture(data, data->textures->tex_s, 's');
+	load_texture_img(data, &data->textures->tex_n, 'n');
+	load_texture_img(data, &data->textures->tex_e, 'e');
+	load_texture_img(data, &data->textures->tex_w, 'w');
+	load_texture_img(data, &data->textures->tex_s, 's');
 	return (1);
 }
