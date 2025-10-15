5384c0f5a58f0f245d25575495065f80914a28d7
6-6-Syj <magand.jessy@outlook.com>
2025-10-15 21:28:31 +0200

    FIX: put_one_pixel_img

--- src/render/draw_pixel.c
+++ src/render/draw_pixel.c
@@ -6,11 +6,19 @@
 /*   By: jmagand <jmagand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 19:51:35 by thmaitre          #+#    #+#             */
-/*   Updated: 2025/10/15 01:13:44 by jmagand          ###   ########.fr       */
+/*   Updated: 2025/10/15 19:31:16 by jmagand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
+
+void	put_one_pixel_img(t_mlx_img *img, int x, int y, int color)
+{
+	char	*pxl;
+
+	pxl = img->img_data + (y * img->size_line + x * (img->bits_per_pixel / 8));
+	*(int *)pxl = color;
+}
 
 void	put_one_pixel(t_data *data, int x, int y, int color)
 {
@@ -19,9 +27,6 @@ void	put_one_pixel(t_data *data, int x, int y, int color)
 	offset = data->mlx_data->mlx_img->img_data + (y
 			* data->mlx_data->mlx_img->size_line + x
 			* data->mlx_data->mlx_img->bits_per_pixel / 8);
-			// (data->mlx_data->mlx_img->size_line
-		// * y) +
-		// (x * (data->mlx_data->mlx_img->bits_per_pixel / 8));
 	*(unsigned int *)offset = color;
 }
 
