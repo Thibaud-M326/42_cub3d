b9133d04f556018f8e4aa34e41096aec29962429
Thibaud Maitre <thmaitre@z2r11p6.42lyon.fr>
2025-10-20 20:06:57 +0200

    ADD: collision in player moove

--- src/hook/player_move.c
+++ src/hook/player_move.c
@@ -6,7 +6,7 @@
 /*   By: thmaitre <thmaitre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 18:06:40 by thmaitre          #+#    #+#             */
-/*   Updated: 2025/10/20 16:25:52 by thmaitre         ###   ########.fr       */
+/*   Updated: 2025/10/20 19:59:50 by thmaitre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ int	player_moove_forward(t_data *data)
 	t_player	*player;
 
 	player = data->player;
-	player->pos_x += player->dir_x * 0.1;
-	player->pos_y += player->dir_y * 0.1;
+	if (data->map->map[(int)(player->pos_y + player->dir_y * 0.8)]
+		[(int)(player->pos_x + player->dir_x * 0.8)] != '1')
+	{
+		player->pos_x += player->dir_x * 0.1;
+		player->pos_y += player->dir_y * 0.1;
+	}
 	return (1);
 }
 
@@ -28,8 +32,12 @@ int	player_moove_backward(t_data *data)
 	t_player	*player;
 
 	player = data->player;
-	player->pos_x -= player->dir_x * 0.1;
-	player->pos_y -= player->dir_y * 0.1;
+	if (data->map->map[(int)(player->pos_y - player->dir_y * 0.8)]
+	[(int)(player->pos_x - player->dir_x * 0.8)] != '1')
+	{
+		player->pos_x -= player->dir_x * 0.1;
+		player->pos_y -= player->dir_y * 0.1;
+	}
 	return (1);
 }
 
@@ -46,8 +54,12 @@ int	player_moove_left(t_data *data)
 	old_dir_y = data->player->dir_y;
 	rot_dir_x = old_dir_x * cos(rad_angle) - old_dir_y * sin(rad_angle);
 	rot_dir_y = old_dir_x * sin(rad_angle) + old_dir_y * cos(rad_angle);
-	data->player->pos_x -= rot_dir_x * 0.1;
-	data->player->pos_y -= rot_dir_y * 0.1;
+	if (data->map->map[(int)(data->player->pos_y - rot_dir_y * 0.8)]
+	[(int)(data->player->pos_x - rot_dir_x * 0.8)] != '1')
+	{
+		data->player->pos_x -= rot_dir_x * 0.1;
+		data->player->pos_y -= rot_dir_y * 0.1;
+	}
 	return (1);
 }
 
@@ -64,8 +76,12 @@ int	player_moove_right(t_data *data)
 	old_dir_y = data->player->dir_y;
 	rot_dir_x = old_dir_x * cos(rad_angle) - old_dir_y * sin(rad_angle);
 	rot_dir_y = old_dir_x * sin(rad_angle) + old_dir_y * cos(rad_angle);
-	data->player->pos_x += rot_dir_x * 0.1;
-	data->player->pos_y += rot_dir_y * 0.1;
+	if (data->map->map[(int)(data->player->pos_y + rot_dir_y * 0.8)]
+	[(int)(data->player->pos_x + rot_dir_x * 0.8)] != '1')
+	{
+		data->player->pos_x += rot_dir_x * 0.1;
+		data->player->pos_y += rot_dir_y * 0.1;
+	}
 	return (1);
 }
 
@@ -75,12 +91,10 @@ int	player_moove(t_data *data)
 		player_moove_forward(data);
 	else if (data->key->s)
 		player_moove_backward(data);
-
 	if (data->key->a)
 		player_moove_left(data);
 	if (data->key->d)
 		player_moove_right(data);
-
 	if (data->key->left)
 		player_turn_left(data);
 	else if (data->key->right)
